d746af71e5ef7e67e75f982bc7586a13438bcb68
Alex Scarlett <alex.scarlett91@gmail.com>
2019-05-10 17:21:23 -0400

    Started work on Rocket script
    Added Rigidbody component, basic input binding, and spacebar now causes rocket to take flight off of launch pad

---
+++ Assets/Rocket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        // separate conditional (can thrust while rotating)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // add force always in direction the ship is facing
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            print("Rotating left");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            print("Rotating right");
+        }
+    }
+}
